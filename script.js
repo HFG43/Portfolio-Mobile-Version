2d96e674c2e6b2b408e5d2f621d85273532325dd
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-03-13 19:44:21 -0500

    Dsiplay Menu

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ let hamburguerMenu = document.querySelector('.fa');
 
 let desktopMenu = document.querySelector('#menu-list');
 
+let menuListItems = document.querySelectorAll('#menu-list > li');
+
+let closeDisplay = document.querySelector('.close-cross');
+
+let backGContainer = document.querySelectorAll('.overlay'); //esta parte aun no logro que ande
+
 function mobileMenu (){
+ 
   hamburguerMenu.classList.add("mobile-menu-display-none"); //desaparece la hamburguesa//
 
   desktopMenu.classList.remove("mobile-hide"); //aparece el menu luego de hacer click en la burguer
 
   desktopMenu.classList.add("list-menu-position"); //posicionamiento del menu
 
+  for(let i = 0; i < menuListItems.length; i++){ 
+  menuListItems[i].classList.add("list-menu-items-font"); //loop para darle estilo a los items de la lista
+  }
+
+  closeDisplay.classList.add("mobile-menu-display-content");
+
+ 
 }
 hamburguerMenu.addEventListener('click', mobileMenu);
