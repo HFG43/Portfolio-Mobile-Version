ac744f00c820786b263263cba0373427aafd68af
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-03-14 22:34:56 -0600

    Spelling Corrected
    All linters aproved

--- script.js
+++ script.js
@@ -1,46 +1,34 @@
-let hamburguerMenu = document.querySelector('.burguer');
-let desktopMenu = document.querySelector('.menu-list');
-let menuListItems = document.querySelectorAll('.menu-list > li >a');
-let portfolioMenu = document.querySelector('.portfolio');
-let aboutMenu = document.querySelector('.about');
-let contactMenu = document.querySelector('.contact');
-let closeDisplay = document.querySelector('.close-icon');
+const hamburguerMenu = document.querySelector('.burguer');
+const desktopMenu = document.querySelector('.menu-list');
+const menuListItems = document.querySelectorAll('.menu-list > li >a');
+const portfolioMenu = document.querySelector('.portfolio');
+const aboutMenu = document.querySelector('.about');
+const contactMenu = document.querySelector('.contact');
+const closeDisplay = document.querySelector('.close-icon');
+const closeCross = document.querySelector('.close-cross');
 
-
-
-function displayListMenu () {
- 
-  hamburguerMenu.classList.add("mobile-menu-display-none");
-  
+function displayListMenu() {
+  hamburguerMenu.classList.add('mobile-menu-display-none');
   desktopMenu.classList.add('mobile-menu-display-content');
- 
   desktopMenu.classList.add('list-menu-position');
-
-  for(let i = 0; i < menuListItems.length; i++){ 
-    menuListItems[i].classList.add('list-menu-items-font'); 
+  for (let i = 0; i < menuListItems.length; i += 1) {
+    menuListItems[i].classList.add('list-menu-items-font');
   }
-  
   closeDisplay.classList.add('mobile-menu-display-content');
 }
-hamburguerMenu.addEventListener('click',displayListMenu);
+hamburguerMenu.addEventListener('click', displayListMenu);
 
-
-
-
-function close () {
-  hamburguerMenu.classList.add("mobile-menu-display-content");
-  desktopMenu.classList.remove('list-menu-position')
-  desktopMenu.classList.add('nav-items-list')
-
-  for(let i = 0; i < menuListItems.length; i++){ 
-    menuListItems[i].classList.add('hide-menu-list-items'); 
-    menuListItems[i].classList.remove('list-menu-items-font'); 
+function close() {
+  hamburguerMenu.classList.add('mobile-menu-display-content');
+  desktopMenu.classList.remove('list-menu-position');
+  desktopMenu.classList.add('nav-items-list');
+  for (let i = 0; i < menuListItems.length; i += 1) {
+    menuListItems[i].classList.add('hide-menu-list-items');
+    menuListItems[i].classList.remove('list-menu-items-font');
   }
-
   closeDisplay.classList.add('hide-close-button');
 }
 portfolioMenu.addEventListener('click', close);
 aboutMenu.addEventListener('click', close);
 contactMenu.addEventListener('click', close);
-closeDisplay.addEventListener('click', close);
-
+closeCross.addEventListener('click', close);
