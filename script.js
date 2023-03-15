d7ec724da31300ab1933e972844a7043333439c0
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-03-15 12:08:03 -0600

    Finished - Ready to Linters Check

--- script.js
+++ script.js
@@ -1,41 +1,20 @@
-  // const hamburguerMenu = document.querySelector('.burguer');
-  // const blurMenu = document.querySelector('.blur-activation');
+  const hamburguerMenu = document.querySelector('.burguer');
+  const blurMenu = document.querySelector('.blur-activation');
+  const portfolioMenu = document.querySelector('.portfolio');
+  const aboutMenu = document.querySelector('.about');
+  const contactMenu = document.querySelector('.contact');
+  const closeDisplay = document.querySelector('.close-icon');
+  const closeCross = document.querySelector('.close-cross');
 
-  // function displayBlurMenu() {
-  //   blurMenu.classList.remove('display-none');
-  // }
-  // hamburguerMenu.addEventListener('click', displayBlurMenu);
+  function displayBlurMenu() {
+    blurMenu.classList.remove('display-none');
+  }
+  hamburguerMenu.addEventListener('click', displayBlurMenu);
 
-// const desktopMenu = document.querySelector('.menu-list');
-// const menuListItems = document.querySelectorAll('.menu-list > li >a');
-// const portfolioMenu = document.querySelector('.portfolio');
-// const aboutMenu = document.querySelector('.about');
-// const contactMenu = document.querySelector('.contact');
-// const closeDisplay = document.querySelector('.close-icon');
-// const closeCross = document.querySelector('.close-cross');
-
-// function displayListMenu() {
-//   hamburguerMenu.classList.add('mobile-menu-display-none');
-//   desktopMenu.classList.add('mobile-menu-display-content');
-//   desktopMenu.classList.add('list-menu-position');
-//   for (let i = 0; i < menuListItems.length; i += 1) {
-//     menuListItems[i].classList.add('list-menu-items-font');
-//   }
-//   closeDisplay.classList.add('mobile-menu-display-content');
-// }
-// hamburguerMenu.addEventListener('click', displayListMenu);
-
-// function close() {
-//   hamburguerMenu.classList.add('mobile-menu-display-content');
-//   desktopMenu.classList.remove('list-menu-position');
-//   desktopMenu.classList.add('nav-items-list');
-//   for (let i = 0; i < menuListItems.length; i += 1) {
-//     menuListItems[i].classList.add('hide-menu-list-items');
-//     menuListItems[i].classList.remove('list-menu-items-font');
-//   }
-//   closeDisplay.classList.add('hide-close-button');
-// }
-// portfolioMenu.addEventListener('click', close);
-// aboutMenu.addEventListener('click', close);
-// contactMenu.addEventListener('click', close);
-// closeCross.addEventListener('click', close);
+  function close() {
+    blurMenu.classList.add('display-none');
+  }
+  portfolioMenu.addEventListener('click', close);
+  aboutMenu.addEventListener('click', close);
+  contactMenu.addEventListener('click', close);
+  closeCross.addEventListener('click', close);
