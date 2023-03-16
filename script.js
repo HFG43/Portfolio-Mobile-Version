5cd40c8f22ff97fba8a5bddf8847f74f40909b04
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-03-16 08:01:51 -0600

    Mobile Pop Up Card Set on place

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-const hamburguerMenu = document.querySelector('.burguer');
-const blurMenu = document.querySelector('.blur-activation');
-const portfolioMenu = document.querySelector('.portfolio');
-const aboutMenu = document.querySelector('.about');
-const contactMenu = document.querySelector('.contact');
-const closeCross = document.querySelector('.close-cross');
+// const hamburguerMenu = document.querySelector('.burguer');
+// const blurMenu = document.querySelector('.blur-activation');
+// const portfolioMenu = document.querySelector('.portfolio');
+// const aboutMenu = document.querySelector('.about');
+// const contactMenu = document.querySelector('.contact');
+// const closeCross = document.querySelector('.close-cross');
 
-function displayBlurMenu() {
-  blurMenu.classList.remove('display-none');
-}
-hamburguerMenu.addEventListener('click', displayBlurMenu);
+// function displayBlurMenu() {
+//   blurMenu.classList.remove('display-none');
+// }
+// hamburguerMenu.addEventListener('click', displayBlurMenu);
 
-function close() {
-  blurMenu.classList.add('display-none');
-}
-portfolioMenu.addEventListener('click', close);
-aboutMenu.addEventListener('click', close);
-contactMenu.addEventListener('click', close);
-closeCross.addEventListener('click', close);
+// function close() {
+//   blurMenu.classList.add('display-none');
+// }
+// portfolioMenu.addEventListener('click', close);
+// aboutMenu.addEventListener('click', close);
+// contactMenu.addEventListener('click', close);
+// closeCross.addEventListener('click', close);
+
+
+const projectMobile1 = {
+  name: "",
+  image: ,
+  frame2: ,
+  description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  languages: ,
+  seeLive: ,
+  seeSource: ,
+  
+};
