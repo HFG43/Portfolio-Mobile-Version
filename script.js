1f087132801229b5f8234b11d499c014abd97511
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-03-16 10:49:31 -0600

    CardsPopUp

--- script.js
+++ script.js
@@ -18,14 +18,47 @@
 // contactMenu.addEventListener('click', close);
 // closeCross.addEventListener('click', close);
 
+const body = document.querySelector('body');
+const popUpMobile = document.createElement('section');
+body.append(popUpMobile);
+section.innerHTML =`    
+<div>
+    <div>
+      <h2></h2>
+      <ul>
+        <li></li>
+        <li><img></li>
+        <li>/li>
+        <li><img></li>
+        <li></li>
+      </ul>
+      <img>
+      <p>hghgfg</p>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    <div>
+      <button><span><img></span></button>
+      <button><span><img></span></button>
+    </div>
+  </div>
+  `;
 
-const projectMobile1 = {
-  name: "",
-  image: ,
-  frame2: ,
-  description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-  languages: ,
-  seeLive: ,
-  seeSource: ,
+const activePopUp = document.querySelector(boton del card original);
+activePopUp.addEventListener('click', function () {
+  const main = document.createElement('div');
+  const showPopUp = document.createElement('div');
+  showPopUp.innerHTML
+}   
+// const projectMobile1 = {
+//   name: "",
+//   image: ,
+//   frame2: ,
+//   description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+//   languages: ,
+//   seeLive: ,
+//   seeSource: ,
   
-};
+// };
