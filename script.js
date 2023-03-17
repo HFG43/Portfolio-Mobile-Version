aad7a17a42c7d99d2b45c4ebc291ac84722e4f3f
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-03-17 11:54:41 -0600

    Changes applied in JS for debug

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const hamburguerMenu = document.querySelector('.burguer');
-const blurMenu = document.querySelector('.blur-activation');
+const blurMenu = document.querySelectorAll('.blur-activation');
 const portfolioMenu = document.querySelector('.portfolio');
 const aboutMenu = document.querySelector('.about');
 const contactMenu = document.querySelector('.contact');
-const closeCross = document.querySelector('.close-cross');
+const closeCross = document.querySelectorAll('.close-cross');
 
-function displayBlurMenu() {
+function displayBlurMenu(addEventListener) {
   blurMenu.classList.remove('display-none');
 }
 hamburguerMenu.addEventListener('click', displayBlurMenu);
 
-function close() {
+function close(portfolioMenu) {
   blurMenu.classList.add('display-none');
 }
-portfolioMenu.addEventListener('click', close);
+portfolioMenu.addEventListener('click');
 aboutMenu.addEventListener('click', close);
 contactMenu.addEventListener('click', close);
 closeCross.addEventListener('click', close);
