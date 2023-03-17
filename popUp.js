5367f9dd3a4d41da1db4e9048f2e259eab33e1c0
HFG43 <117187653+HFG43@users.noreply.github.com>
2023-03-16 19:30:56 -0600

    PopUp JS

--- popUp.js
+++ popUp.js
@@ -34,57 +34,59 @@ const openProject4 = document.querySelector('.see-more-project-4');
 
 let project = undefined;
 function clickProj1() {
-    projects = project1;
-    console.log(project1);
-  }
-  openProject1.addEventListener('click', clickProj1);
+    project = project1;
+    console.log(project);
+    return project;
+}
+  openProject1.addEventListener('click', clickProj1, popUpRun);
 
 function clickProj2() {
     project = project2;
     console.log(project2);
+    return project;
 }
   openProject2.addEventListener('click', clickProj2);
 
 function clickProj3() {
     project = project3;
-    console.log(project3);
+    return project;
 }
   openProject3.addEventListener('click', clickProj3);
 
 function clickProj4() {
     project = project4;
-    console.log(project4);
+    return project;
 }
 openProject4.addEventListener('click', clickProj4);
 
-  project.addEventListener('click', function (popUpRun) {
-  const body = document.querySelector('body');
-  const popUpMobile = document.createElement('section');
-  body.appendChild(popUpMobile);
-  section.innerHTML =`    
-  <div class="card-works">
-    <div class="card-work-insider">
-      <h2>"${project.title}"</h2>
-      <li><img src="Images/Closing-cross.png" class="close"></li>
-      <ul class="frame-2">
-        <li>"${project.frame1}"</li>
-        <li><img src="${project.frame2.src}"></li>
-        <li>"${project.frame3}"</li>
-        <li><img src="${project.frame4.src}"></li>
-        <li>"${project.frame5}"</li>
-      </ul>
-      <img src="${project.snapshot.src}"></li>>
-      <p class="primary-project-text">"${project.paragraph}"</p>
-      <ul class="tags-container">
-        <li>"${project.languages1}"</li>
-        <li>"${project.languages2}"</li>
-        <li>"${project.languages3}"</li>
-        <li>"${project.languages4}"</li>
-      </ul>
-      <div class="button-mobile-pop-up-container">
-        <button href="#">See live<span><img src="Images/See-live-icon.svg"></span></button>
-        <button href="#">See source<span><img src="/Images/smm-icons/github-Icon.svg"></span></button>
+function popUpRun() {
+    const body = document.querySelector('body');
+    const popUp = document.createElement('div');
+    body.appendChild(popUp);
+    popUp.innerHTML =`
+    <div class="card-works">
+      <div class="card-work-insider">
+        <h2>"${project.title}"</h2>
+        <li><img src="Images/Closing-cross.png" class="close"></li>
+        <ul class="frame-2">
+          <li>"${project.frame1}"</li>
+          <li><img src="${project.frame2.src}"></li>
+          <li>"${project.frame3}"</li>
+          <li><img src="${project.frame4.src}"></li>
+          <li>"${project.frame5}"</li>
+        </ul>
+        <img src="${project.snapshot.src}"></li>>
+        <p class="primary-project-text">"${project.paragraph}"</p>
+        <ul class="tags-container">
+          <li>"${project.languages1}"</li>
+          <li>"${project.languages2}"</li>
+          <li>"${project.languages3}"</li>
+          <li>"${project.languages4}"</li>
+        </ul>
+        <div class="button-mobile-pop-up-container">
+          <button href="#">See live<span><img src="./Images/See-live-icon.svg"></span></button>
+          <button href="#">See source<span><img src="./Images/smm-icons/github-Icon.svg"></span></button>
+      </div>
     </div>
-  </div>
-  `;
-})
+    `;
+} 
